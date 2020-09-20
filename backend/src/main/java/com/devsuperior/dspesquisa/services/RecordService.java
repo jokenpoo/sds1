82d24fee4d453246760b5ed0ab4de330bb5ce9b6
Gustavo Jung <jung.gustavo@gmail.com>
2020-09-20 09:26:12 -0700

    GET records

--- backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -3,6 +3,8 @@ package com.devsuperior.dspesquisa.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,27 +17,32 @@ import com.devsuperior.dspesquisa.repositories.RecordRepository;
 
 @Service
 public class RecordService {
-	
+
 	@Autowired
 	private RecordRepository repository;
-	
+
 	@Autowired
 	private GameRepository gameRepository;
-	
+
 	@Transactional
 	public RecordDTO insert(RecordInsertDTO dto) {
-		
-	Record entity = new Record();
-	
-	entity.setName(dto.getName());
-	entity.setAge(dto.getAge());
-	entity.setMoment(Instant.now());
-	
-	Game game = gameRepository.getOne(dto.getGameID());
-	entity.setGame(game);
-	
-	entity = repository.save(entity);
-	return new RecordDTO(entity);
-	
+
+		Record entity = new Record();
+
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+
+		Game game = gameRepository.getOne(dto.getGameID());
+		entity.setGame(game);
+
+		entity = repository.save(entity);
+		return new RecordDTO(entity);
+
+	}
+
+	@Transactional(readOnly = true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		return repository.findByMoments(minDate, maxDate, pageRequest).map(x -> new RecordDTO(x));
 	}
 }
