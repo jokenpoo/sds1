5a645dea92ab735024477120171267c28306faf6
Gustavo Jung <jung.gustavo@gmail.com>
2020-09-20 18:11:29 -0700

    Adicionando a pagina de gráficos

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Charts from './pages/Charts';
 import Home from './pages/Home';
 import Records from './pages/Records';
 
@@ -13,6 +14,10 @@ const Routes = () => (
             </Route>
             <Route path="/records">
                 <Records/>
+            </Route>
+
+            <Route path="/charts">
+                <Charts/>
             </Route>
         </Switch>
     </BrowserRouter>
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -5,7 +5,7 @@ import './styles.css';
 import { RecordsResponse } from './types';
 import { formatDate } from './helpers';
 import Pagination from './Pagination';
-import { Link } from 'react-router-dom';
+import Filters from '../../components/Header/Filters';
 
 const BASE_URL = 'http://localhost:8080';
 
@@ -25,14 +25,7 @@ const Records = () => {
 
     return (
     <div className="page-container">
-        <div className="filters-container records-actions">
-            <Link to="/charts">
-                <button className="action-filters">
-                    VER GRÁFICO                    
-                </button>
-            </Link>
-
-        </div>
+        <Filters link="/charts" linkText="VER GRÁFICO"/>
         <table className="records-table" cellPadding="0" cellSpacing="0">
             <thead>
                 <tr>
